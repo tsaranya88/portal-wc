45c1af37df4a79cbbab9471b9efa97b01ecae79e
tsaranya88 <arunkvar@gmail.com>
2020-07-03 20:42:49 -0500

    Fix bugs.

--- src/app/custom/test-element.ts
+++ src/app/custom/test-element.ts
@@ -2,12 +2,12 @@ import { LitElement, html, css, customElement, property } from 'lit-element';
 
 @customElement('test-element')
 export class TestElement extends LitElement {
-  static styles = css`h1 { color: orange; }`;
+  static styles = css`span { color: orange; }`;
 
   @property({ type: String, reflect: true })
   customerName = 'world';
 
   render() {
-    return html`<h1>Showing details of ${this.customerName}!</h1>`;
+    return html`<h1>Showing details of <span>${this.customerName}</span>!</h1>`;
   }
 }
